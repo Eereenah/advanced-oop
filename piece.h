3f3e0aac1a3bc2cbb7869830c2b528f3c28ea644
Eereenah <Eereenah@users.noreply.github.com>
2019-12-05 00:55:34 +0400

    added piece header file

---
+++ piece.h
@@ -0,0 +1,30 @@
+#pragma once
+
+enum PieceType {
+	PAWN,
+	BISHOP,
+	KNIGHT,
+	ROOK,
+	QUEEN,
+	KING
+};
+
+class Piece {
+public:
+	virtual int getCurrentLocation() = 0;
+
+	virtual bool getColor() = 0;
+
+	virtual PieceType getType() = 0;
+
+	virtual void setCurrentLocation(int) = 0;
+
+	virtual bool isPossibleMove(int) = 0;
+
+	virtual std::set<int> getBlockedPosition(int, int) = 0;
+
+private:
+	virtual void generatePossibleMoves(int) = 0;
+
+	virtual std::shared_ptr<std::set<int>> getPossibleMoves() = 0;
+};
