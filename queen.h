2f1639d643826d5c3f03b49e8e0a44bd6f6a6fa6
Eereenah <Eereenah@users.noreply.github.com>
2019-12-08 19:32:32 +0400

    added two more pieces (header files)

---
+++ queen.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef QUEEN_H
+#define QUEEN_H
+
+#include "piece.h"
+
+#include <set>
+#include <memory>
+
+class Queen : public Piece {
+public:
+	Queen(int location, bool c);
+	int getCurrentLocation();
+	bool getColor();
+	PieceType getType();
+	void setCurrentLocation(int);
+	bool isPossibleMove(int);
+	std::set<int> getBlockedPosition(int, int);
+private:
+	bool color;
+	PieceType type;
+	int currentLocation;
+	std::shared_ptr<std::set<int>> possibleMoves;
+
+	void generatePossibleMoves(int);
+};
+
+#endif
---
+++ rook.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef ROOK_H
+#define ROOK_H
+
+#include "piece.h"
+
+#include <set>
+#include <memory>
+
+class Rook : public Piece {
+public:
+	Rook(int location, bool c);
+	int getCurrentLocation();
+	bool getColor();
+	PieceType getType();
+	void setCurrentLocation(int);
+	bool isPossibleMove(int);
+	std::set<int> getBlockedPosition(int, int);
+private:
+	bool color;
+	PieceType type;
+	int currentLocation;
+	std::shared_ptr<std::set<int>> possibleMoves;
+
+	void generatePossibleMoves(int);
+};
+
+#endif
