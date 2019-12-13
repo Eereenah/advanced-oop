387ca31337ad1c446f9a8be114e37ea2554de924
Eereenah <Eereenah@users.noreply.github.com>
2019-12-13 23:31:47 +0400

    added header files for helper classes

---
+++ command.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include "piece.h"
+#include "string"
+
+enum CommandType {
+	INVALID,
+	LOAD,
+	SAVE,
+	QUIT,
+	CONTROL,
+	RESTART
+};
+
+struct Command {
+	Command() = default;
+	Command(CommandType t, std::string in, int ori = 0, int des = 0) :
+		type(t), origin(ori), dest(des), raw(in) { }
+	CommandType type;
+	int origin;
+	int dest;
+	std::string raw;
+};
+#endif
---
+++ utility.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <utility>
+
+using std::pair;
+
+// 1d to 2d: pair<file, rank>, ex: 36 -> pair<4, 5>
+pair<int, int> to2d(int loc);
+
+// 2d to 1d
+int to1d(int f, int r);
+
+#endif
